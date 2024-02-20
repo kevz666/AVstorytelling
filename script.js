61b003415347c3a62958114cd9e7953f5308e9be
kevz666 <67724051+kevz666@users.noreply.github.com>
2024-02-19 17:14:45 -0800

    edits

--- script.js
+++ script.js
@@ -13,4 +13,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
       // Hide the overlay when playing
       this.closest('.overlay').style.display = 'none';
   });
+
+  document.getElementById('yieldButton').addEventListener('click', function() {
+    // Change the video source to 'My Movie 11.mp4'
+    video.src = 'path/to/My Movie 11.mp4';
+    video.play();
+    // Hide the overlay when playing
+    this.closest('.overlay').style.display = 'none';
+});
+
+  // Event listener for the continue ride button
+  document.getElementById('continueRideButton').addEventListener('click', function() {
+    video.src = 'path/to/My Movie 11.mp4';
+    video.load(); // Load the new source
+    video.onloadeddata = function() {
+        video.play();
+        this.closest('.overlay').style.display = 'none';
+    };
+});
 });

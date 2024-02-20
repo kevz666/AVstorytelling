63a49fa7ff47eca09661d36150bd620f8fadd4d9
kevz666 <67724051+kevz666@users.noreply.github.com>
2024-02-19 16:47:13 -0800

    added second video prompt

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-document.getElementById('startRideButton').addEventListener('click', function() {
-    var video = document.getElementById('myVideo');
-    video.play();
-  
-    // Hide the overlay when playing
-    this.closest('.overlay').style.display = 'none';
-  });
-  
-<script>
-    document.addEventListener('DOMContentLoaded', (event) => {
-        const video = document.getElementById('myVideo');
-        const overlay = document.getElementById('videoOverlay');
+document.addEventListener('DOMContentLoaded', (event) => {
+  const video = document.getElementById('myVideo');
+  const overlay = document.getElementById('videoOverlay');
 
-        video.onended = function() {
-            overlay.style.display = 'flex';
-        };
-    });
-</script>
+  // Event listener for when the video ends
+  video.onended = function() {
+      overlay.style.display = 'flex';
+  };
+
+  // Event listener for the start ride button
+  document.getElementById('startRideButton').addEventListener('click', function() {
+      video.play();
+      // Hide the overlay when playing
+      this.closest('.overlay').style.display = 'none';
+  });
+});
